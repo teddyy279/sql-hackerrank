20ca4e9099deec65b1c458afbb9f891e876cef23
teddyy279 <151600961+teddyy279@users.noreply.github.com>
2025-09-11 23:53:53 +0700

    Create Weather Observation Station 19.sql

---
+++ Weather Observation Station 19.sql
@@ -0,0 +1,9 @@
+SELECT CAST(
+    ROUND(
+        SQRT(
+            POWER(ABS(MIN(LAT_N) - MAX(LAT_N)), 2)
+            + POWER(ABS(MIN(LONG_W) - MAX(LONG_W)), 2)
+        ), 4
+    ) AS DECIMAL(18, 4)
+)
+FROM STATION
